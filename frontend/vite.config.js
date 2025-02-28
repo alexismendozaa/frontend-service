fe3abe989f36ee67ccc0cfd3c693b346898a7902
Alexis <afmendozaf@uce.edu.ec>
2025-02-28 12:41:51 -0500

    asddddd

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,8 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    open: false, // Cambiado a 'false' para evitar que Vite intente abrir el navegador
+    open: false,  // Para evitar que intente abrir el navegador automáticamente
+    host: '0.0.0.0',  // Esto permitirá que Vite escuche en todas las interfaces
+    port: 5173,  // El puerto en el que Vite estará escuchando
   },
 });

3729e545348d66d7914d4c240ffed717de34c778
alexismendozaa <afmendozaf@uce.edu.ec>
2025-03-07 15:45:34 -0500

    docs: 📝 improve code comments

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,8 +4,8 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    open: false,  // Para evitar que intente abrir el navegador automáticamente
-    host: '0.0.0.0',  // Esto permitirá que Vite escuche en todas las interfaces
-    port: 5173,  // El puerto en el que Vite estará escuchando
+    open: false, 
+    host: '0.0.0.0',  
+    port: 5173,  
   },
 });
